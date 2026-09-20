c8bd9ee5faff92698e1b4e98ffa7b06649a2c889
Jie <1614094+yagay@users.noreply.github.com>
2026-09-20 07:03:27 +0100

    Align Kotlin and AGP with GeckoView 155

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
-    id("com.android.application") version "9.2.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
+    id("com.android.application") version "9.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
