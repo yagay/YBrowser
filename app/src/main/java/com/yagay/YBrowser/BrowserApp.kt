e5ef3821d8a81b31cfcaa94dc5d169c634f9aa8d
Jie <1614094+yagay@users.noreply.github.com>
2026-09-20 08:23:38 +0100

    Fix tab preview size import

--- app/src/main/java/com/yagay/YBrowser/BrowserApp.kt
+++ app/src/main/java/com/yagay/YBrowser/BrowserApp.kt
@@ -33,6 +33,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
