88f73bedf03777010338f44a66051fdb535b89bd
Jie <1614094+yagay@users.noreply.github.com>
2026-09-22 15:01:54 +0100

    Add KSP for Room conversation storage

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.library") version "9.4.0" apply false
     id("org.jetbrains.kotlin.android") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+    id("com.google.devtools.ksp") version "2.3.12" apply false
 }
