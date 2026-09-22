5bc9a775e5ca0441f2d28533840a216380946b40
Jie <1614094+yagay@users.noreply.github.com>
2026-09-22 15:24:39 +0100

    Bump YBrowser to 0.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yagay.YBrowser"
         minSdk = 31
         targetSdk = 37
-        versionCode = 15
-        versionName = "0.8.0"
+        versionCode = 16
+        versionName = "0.8.1"
     }
 
     buildFeatures {
