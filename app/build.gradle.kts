51c5b3d3ea32860ad883a7104f2aedef6e071656
Jie <1614094+yagay@users.noreply.github.com>
2026-09-24 14:05:28 +0800

    Add Google Cast sender framework

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("androidx.webkit:webkit:1.15.0")
     implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
+    implementation("com.google.android.gms:play-services-cast-framework:22.3.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
